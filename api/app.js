94e57834ea93126c7d61cc8b3a6d2c26b13ec3b7
zunm3133 <moemoegyi1234567890@gmail.com>
2026-01-04 14:11:10 +0800

    first commit

---
+++ api/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+const app = express();
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.listen(8800, () => {
+    console.log("Server is working");
+});
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+export const register = async(req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
