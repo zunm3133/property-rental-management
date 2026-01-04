0604d51488cd7d93c76db5c357dfbb314f54648f
zunm3133 <moemoegyi1234567890@gmail.com>
2026-01-04 14:47:44 +0800

    good

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -14,6 +14,7 @@ const HomePage = () => {
       </div>
       <div className="imgContainer">
         <img src="/property.png" alt="" />
+        <div>SOoo goodd</div>
       </div>
     </div>
   );
